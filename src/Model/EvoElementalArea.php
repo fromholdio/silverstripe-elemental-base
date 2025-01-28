8a31351f342c722600ea43352fc181b45aa62335
Florian Thoma <florian.thoma@innoweb.com.au>
2025-01-28 14:13:28 +1100

    check current element for match

--- src/Model/EvoElementalArea.php
+++ src/Model/EvoElementalArea.php
@@ -513,6 +513,10 @@ class EvoElementalArea extends ElementalArea
         $areaElements = $this->getAllElements();
         foreach ($areaElements as $areaElement)
         {
+            if ($areaElement->ID === $id) {
+                $element = $areaElement;
+                break;
+            }
             $providedElements = $areaElement->provideElements();
             if (!is_null($providedElements)) {
                 $element = $providedElements->find('ID', $id);
