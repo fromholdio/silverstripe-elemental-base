c889d72beae5a6c66176daa1e6289c845ac4c433
Florian Thoma <florian.thoma@innoweb.com.au>
2025-02-12 17:04:06 +1100

    add 'Publish including blocks' button to pages managed in a ModelAdmin

--- src/Extensions/GridFieldDetailFormItemRequestExtension.php
+++ src/Extensions/GridFieldDetailFormItemRequestExtension.php
@@ -3,7 +3,10 @@
 namespace Fromholdio\Elemental\Base\Extensions;
 
 use DNADesign\Elemental\Models\BaseElement;
+use LeKoala\CmsActions\CustomAction;
 use SilverStripe\Core\Extension;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\ORM\DataObject;
 use SilverStripe\ORM\FieldType\DBField;
 
 class GridFieldDetailFormItemRequestExtension extends Extension
@@ -20,5 +23,42 @@ class GridFieldDetailFormItemRequestExtension extends Extension
                 $record->getType()
             ));
         }
+    }
+
+    public function updateFormActions(FieldList $actions)
+    {
+        // Check if record is versionable
+        /** @var DataObject $record */
+        $record = $this->getOwner()->getRecord();
+        $ownerHasAreas = $record
+            && $record->hasMethod('getElementalAreas')
+            && $record->getElementalAreas();
+
+        // Add extra actions prior to extensions so that these can be modified too
+        if ($ownerHasAreas) {
+            $action = CustomAction::create('doPublishWithAreas', 'Publish (including all blocks)')
+                ->setShouldRefresh(true)
+                ->addExtraClass('btn-outline-primary')
+                ->removeExtraClass('btn-info');
+
+            $majorActions = $actions->fieldByName('MajorActions');
+
+            if ($majorActions) {
+                $majorActions->push($action);
+            } else {
+                $actions->push($action);
+            }
+        }
+    }
+
+    public function doPublishWithAreas($data, $form)
+    {
+        /** @var DataObject $record */
+        $record = $this->getOwner()->getRecord();
+
+        $record->doPublishWithAreas();
+
+        // Redirect back to edit
+        return $this->getOwner()->redirectAfterSave(false);
     }
 }
